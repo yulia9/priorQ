5d9c26941f74164a34fedc61da03461b29c1c84d
Yulia Samuilik <usamuilik@gmail.com>
2016-08-20 22:47:29 +0200

    second commit

--- src/node.js
+++ src/node.js
@@ -1,9 +1,15 @@
 class Node {
 	constructor(data, priority) {
-
+this.data = data;
+this.priority = priority;
+this.parent=null;
+this.left=null;
+this.right=null;
 	}
 
 	appendChild(node) {
+var node = new Node (data, priority);
+	
 
 	}
 
